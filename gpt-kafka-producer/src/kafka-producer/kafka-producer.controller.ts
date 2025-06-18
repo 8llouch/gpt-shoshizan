4b6ebbc4c07ce750fbabab708be17eae058256be
8llouch <w.martinache@gmail.com>
2025-06-18 15:07:22 +0200

    refactor: apply jwt guard on controllers and add userId from strategy to msg

--- gpt-kafka-producer/src/kafka-producer/kafka-producer.controller.ts
+++ gpt-kafka-producer/src/kafka-producer/kafka-producer.controller.ts
@@ -1,26 +1,41 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { KafkaProducerService } from './kafka-producer.service';
 import { LlmRequestMessageDto } from './dto/llm-request-message.dto';
-import { LlmRequestMessage } from '@shoshizan/shared-interfaces';
+import { LlmRequestMessage, JwtPayload } from '@shoshizan/shared-interfaces';
+import { JwtAuthGuard } from '../common/guards/jwt-authentication.guard';
 
 @Controller('message-producer')
 export class KafkaProducerController {
-  constructor(private readonly producerService: KafkaProducerService) {}
+  constructor(
+    private readonly producerService: KafkaProducerService,
+    private readonly jwtService: JwtService,
+  ) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('ai-inputs')
-  async sendUserInput(@Body() input: LlmRequestMessageDto) {
+  async sendUserInput(
+    @Body() input: LlmRequestMessageDto,
+    @Request() req: { user: JwtPayload },
+  ) {
     const message: LlmRequestMessage = {
       ...input,
       timestamp: new Date().toISOString(),
+      userId: req.user.sub,
     };
     return this.producerService.produceMessage('input.created', message);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('ai-outputs')
-  async sendAiOutput(@Body() output: LlmRequestMessageDto) {
+  async sendAiOutput(
+    @Body() output: LlmRequestMessageDto,
+    @Request() req: { user: JwtPayload },
+  ) {
     const message: LlmRequestMessage = {
       ...output,
       timestamp: new Date().toISOString(),
+      userId: req.user.sub,
     };
     return this.producerService.produceMessage('output.created', message);
   }
