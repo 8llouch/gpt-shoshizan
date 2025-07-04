a389f3e09b43a20d60f98af582a6f9750be008dc
8llouch <w.martinache@gmail.com>
2025-07-04 14:32:53 +0200

    refactor: remove Swagger configuration for security concerns

--- gpt-kafka-producer/src/main.ts
+++ gpt-kafka-producer/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,15 +13,6 @@ async function bootstrap() {
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
-  // Swagger configuration
-  const config = new DocumentBuilder()
-    .setTitle('Shoshizan API')
-    .setDescription('The Shoshizan API documentation')
-    .setVersion('1.0')
-    .addTag('API Requests')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
   // Start HTTP server
   await app.listen(Number(process.env.PRODUCER_PORT));
